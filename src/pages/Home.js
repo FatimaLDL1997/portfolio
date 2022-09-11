64926638f799c7a51a7c13bd5e32ceded0717da0
Fatima Labade <fatima.labade@ontariotechu.net>
2022-09-11 18:47:58 -0400

    fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,19 +10,13 @@ const Home = () => {
   };
   return (
     <section className='pageContainer' onWheel={handleScroll}>
-      <h1 className='home-title'>Welcome to My Page!</h1>
+      <h1 className='home-title'>Fatima Labade</h1>
+      <h5 className='subtitle'>Web Developer, Artist and Engineer</h5>
       <img src='/images/code-girl.png' className='my-image' alt='' />
       <h5 className='home-discription'>
         I'm a Mechatronics Engineer skilled in web development with a very
-        strong background in Robotics programming. I recently graduated in April
-        2021 and I am looking for an entry-level engineering job to begin my
-        career journey.
-      </h5>
-      <h5 className='home-discription'>
-        I am currently focused on improving my web development skills through
-        projects like this website and online courses. My long-term career goal
-        is to become a Web Developer, or to work on the design, development and
-        programming of sensors and robotics.
+        strong background in Robotics programming. I recently graduated and I am
+        looking for an entry-level engineering job to jumpstart my career.
       </h5>
     </section>
   );
